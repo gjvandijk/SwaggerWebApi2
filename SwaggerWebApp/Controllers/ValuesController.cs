fa359b15320d85deb386edcf4a2ab035725dfff4
Gert Jan van Dijk <gjvandijk@home.nl>
2018-05-03 17:01:56 +0200

    Modified values controller Nuget package Swashbuckle.Net45 installed

--- SwaggerWebApp/Controllers/ValuesController.cs
+++ SwaggerWebApp/Controllers/ValuesController.cs
@@ -9,29 +9,34 @@ namespace SwaggerWebApp.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        private string[] lista
+        {
+            get
+            {
+                if (System.Web.HttpContext.Current.Application["lista"] == null)
+                {
+                    System.Web.HttpContext.Current.Application["lista"] =
+                    new string[] {"value1", "value2"};
+                }
+                return
+                    (string[])System.Web.HttpContext.Current.Application["lista"];
+            }
+        }
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return lista;
         }
-
-        // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return lista[id];
         }
-
-        // POST api/values
         public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            lista[id] = value;
         }
-
-        // DELETE api/values/5
         public void Delete(int id)
         {
         }
