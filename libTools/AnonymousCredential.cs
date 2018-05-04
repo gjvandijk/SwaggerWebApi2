628a730210cba3038fbd4094bbff21d04ba95bc5
Gert Jan van Dijk <gjvandijk@home.nl>
2018-05-04 12:22:56 +0200

    - added package Microsoft.Rest.ClientRuntime to libtools for solving anonymous authentication problem. - added Libtools/AnonymousCredential class

---
+++ libTools/AnonymousCredential.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+namespace libTools
+{
+    /// <summary>
+    /// Web API proxy needs an additional class to control authentication,
+    /// even if no authentication is required. 
+    /// Quick fix is to implement the ServiceClientCredentials interface with 
+    /// an empty implementation class cof ServiceClientCredentials to control authentication
+    /// . 
+    /// </summary>
+    public class AnonymousCredential : ServiceClientCredentials
+    {
+    }
+}
